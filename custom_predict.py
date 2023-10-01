5519a0200f8d142b5d756f53607d5c61ca3167df
BeeJayK <marlin.berger@posteo.de>
2023-10-01 13:30:22 +0200

    feat: add visualizations, start with conversion settings

--- custom_predict.py
+++ custom_predict.py
@@ -1,8 +1,11 @@
 # %%
+import coremltools as ct
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import pathlib
 import torch
+import torchvision
 
 from models.network_swinir import SwinIR as net
 
@@ -10,33 +13,47 @@ from models.network_swinir import SwinIR as net
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 private_model_zoo = {
-    'classical_2x_M': {
-        'file_name': '001_classicalSR_DF2K_s64w8_SwinIR-M_x2.pth',
-        'model_type': 'classical_sr',
-        'scale': 2,
-        'large_model': False,
-    },
-
-    'classical_2x_light': {
+    'light_2x': {
         'file_name': '002_lightweightSR_DIV2K_s64w8_SwinIR-S_x2.pth',
         'model_type': 'lightweight_sr',
         'scale': 2,
         'large_model': False,
     },
+    'light_4x': {
+        'file_name': '002_lightweightSR_DIV2K_s64w8_SwinIR-S_x4.pth',
+        'model_type': 'lightweight_sr',
+        'scale': 4,
+        'large_model': False,
+    },
 
-    'real_2x_light': {
+    'real_2x_M': {
         'file_name': '003_realSR_BSRGAN_DFO_s64w8_SwinIR-M_x2_PSNR.pth',
         'model_type': 'real_sr',
         'scale': 2,
         'large_model': False,
     },
+    'real_4x_L': {
+        'file_name': '003_realSR_BSRGAN_DFOWMFC_s64w8_SwinIR-L_x4_PSNR.pth',
+        'model_type': 'real_sr',
+        'scale': 4,
+        'large_model': True,
+    },
+    'real_4x_L_GAN': {
+        'file_name': '003_realSR_BSRGAN_DFOWMFC_s64w8_SwinIR-L_x4_GAN.pth',
+        'model_type': 'real_sr',
+        'scale': 4,
+        'large_model': True,
+    },
 }
 
 
 private_image_zoo = {
-    't1': {
+    'g1': {
         'file_name': 'F0D9843E-5F6C-47FA-8172-F74E3423F321_1_105_c.jpeg'
-    }
+    },
+    'g2': {
+        'file_name': 'IMG_1732.jpeg'
+    },
 }
 
 
@@ -54,21 +71,9 @@ def get_model(model_name):
     upsale = private_model_zoo[model_name]['scale']
     large_model = private_model_zoo[model_name]['large_model']
 
-    # look up in description in main_test_swinir; seems not to be important
-    # parameter
-    default_classical_sr_training_patch_size = 128
-
-    if model_type == 'classical_sr':
-        model = net(upscale=upsale, in_chans=3,
-                    img_size=default_classical_sr_training_patch_size,
-                    window_size=8, img_range=1., depths=[6, 6, 6, 6, 6, 6],
-                    embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2,
-                    upsampler='pixelshuffle', resi_connection='1conv')
-        param_key_g = 'params'
-
     # 002 lightweight image sr
     # use 'pixelshuffledirect' to save parameters
-    elif model_type == 'lightweight_sr':
+    if model_type == 'lightweight_sr':
         model = net(upscale=upsale, in_chans=3, img_size=64, window_size=8,
                     img_range=1., depths=[6, 6, 6, 6], embed_dim=60,
                     num_heads=[6, 6, 6, 6],
@@ -139,7 +144,7 @@ def preprocess_following_original_git(img_lq):
     return img_lq
 
 
-def get_image(image_name):
+def get_torch_image(image_name):
     image_file_name = private_image_zoo[image_name]['file_name']
     private_image_zoo_path = pathlib.Path(
         '/Users/marlinberger/Desktop/local/Coding/SwinIR/image_zoo'
@@ -149,22 +154,63 @@ def get_image(image_name):
     return preprocess_following_original_git(img)
 
 
-def predict(model_name, image_name):
-    model = get_model(model_name)
-    img = get_image(image_name)
+def imshow_image(torch_like):
+    PIL_img = torchvision.transforms.functional.to_pil_image(
+        torch_like[0, :, :, :]
+    )
+    f, ax = plt.subplots()
+    ax.imshow(PIL_img)
+    ax.axis('off')
 
-    print(img.shape)
+
+def visualize_results(orig_img, output):
+    imshow_image(orig_img)
+    imshow_image(output)
+
+
+def predict(model_name, image_name, with_visualization=True):
+    model = get_model(model_name)
+    img = get_torch_image(image_name)
 
     with torch.no_grad():
         output = model(img)
 
-    print(output.shape)
+    if with_visualization:
+        visualize_results(img, output)
+        plt.show()
 
+
+def convert_to_apple(model_name, image_name):
+    model = get_model(model_name)
+    img = get_torch_image(image_name)
+    # Trace the model with desired data
+    traced_model = torch.jit.trace(model, img)
+    _ = traced_model(img)
+
+    # Using image_input in the inputs parameter:
+    # Convert to Core ML program using the Unified Conversion API.
+    model = ct.convert(
+        traced_model,
+        convert_to="mlprogram",
+        inputs=[ct.TensorType(shape=img.shape)]
+    )
+    # Save the converted model.
+    model.save("newmodel.mlpackage")
 
 
 if __name__ == '__main__':
-    predict(
-        'classical_2x_light',
-        't1'
+    model_name = 'light_2x'
+    test_image = 'g1'
+
+    # predict(
+    #     model_name,
+    #     test_image
+    # )
+
+    convert_to_apple(
+        model_name,
+        test_image
     )
+
+
 # %%
